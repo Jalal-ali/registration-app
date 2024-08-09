653f99ae0b5acbefacdcf9baf7735f11fc428d9f
Jallluu <alikhanumar795@gmail.com>
2024-08-10 02:27:42 +0500

    updated

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
-import { signOut } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+// import { getAuth,  } from "firebase/auth";
+import { signOut , onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { auth } from "./config.js";
 const btn = document.querySelector("#btn");
 
@@ -12,3 +13,16 @@ btn.addEventListener("click" , ()=>{
     });
 
 })
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    // ...
+  } else {
+    window.location = "index.html" ;
+    // ...
+  }
+});
